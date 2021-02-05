3b7f679ae7237995323624312881228e46abccb0
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-02-05 09:22:45 +0330

    Computed Properties And Refactor Any Method

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 var app = new Vue({
     el:'#app',
     data:{
-        altText : 'عکس جوراب',
-        product : 'جوراب',
-        image: './asset/vmSocks-green-onWhite.jpg',
-        inventory:0,
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        cart: 0,
         onSale: true,
-        details:["80% Cotton","20% polyester","Gender-neurtal"],
-        cart:0,
         variants:[
             {
               variantId: 2234,
               variantColor: 'green',
               variantImage:'./asset/vmSocks-green-onWhite.jpg',
+              variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage:'./asset/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0   
             }
             
         ]
@@ -26,8 +27,25 @@ var app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage){
-            this.image = variantImage
+        updateProduct(index){
+            this.selectedVariant = index
         }
+    },
+    computed:{
+        title() {
+            return this.brand + ' ' + this.product  
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+              return this.brand + ' ' + this.product + ' are on sale!'
+            } 
+              return  this.brand + ' ' + this.product + ' are not on sale'
+          }
     }
 })
